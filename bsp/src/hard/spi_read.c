8f4ce6bac826139725c63a71a3eca20c3538f858
MinhHac <minhhac3195@gmail.com>
2020-04-27 20:47:10 +0200

    Delete spi_read.c

--- bsp/src/hard/spi_read.c
+++
@@ -1,9 +0,0 @@
-#include <spi/include/spi.h>
-
-void spi_read(uInt8_t *payload)
-{    
-    SSPBUF=0xff;		/* Copy flush byte in SSBUF */
-    while(!PIR1bits.SSPIF);	/* Wait for complete 1 byte transmission */
-    PIR1bits.SSPIF=0;
-    *payload = SSPBUF;
-}
